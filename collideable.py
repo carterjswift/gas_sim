fcd769380f19a5107457487b94d9755ae522c7ab
carterjswift <carterjswift@gmail.com>
2020-04-04 22:51:22 -0500

    fixed bug of particles leaking out of box

--- collideable.py
+++ collideable.py
@@ -1,6 +1,8 @@
 import sys
 from abc import ABC, abstractmethod
 import numpy as np
+import math
+
 
 class Collideable(ABC):
 
@@ -8,27 +10,28 @@ class Collideable(ABC):
     def collide(self, other):
         pass
 
+
 class Wall(Collideable):
 
     def __init__(self, norm, pos):
-        #make sure normal vector of wall is normalized
-        self.norm = np.divide(norm,np.linalg.norm(norm))
+        # make sure normal vector of wall is normalized
+        self.norm = np.divide(norm, np.linalg.norm(norm))
 
         self.pos = pos
 
     def collide(self, other):
-        if isinstance(other,Wall):
+        if isinstance(other, Wall):
             return
 
-        #perform collision
+        # perform collision
         initialv = other.v
-        v_par = np.multiply(self.norm,np.dot(initialv, self.norm) / np.dot(self.norm, self.norm))
-        v_perp = initialv - v_par
-        other.v = np.subtract(v_perp, v_par)
-
+        v_par = np.multiply(self.norm, np.dot(initialv, self.norm))
+        deltaV = np.multiply(-2,v_par)
+        other.v = np.add(initialv, deltaV)
 
         # Return the change in momentum of the particle
-        return np.multiply(other.mass, np.subtract(other.v,initialv))
+        return np.linalg.norm(np.multiply(other.mass,deltaV))
+
 
 class Atom(Collideable):
 
@@ -37,82 +40,83 @@ class Atom(Collideable):
         self.radius = radius
         self.pos = pos
         self.v = v
-    
-    def move(self, time):
-        self.pos = np.add(self.pos,np.multiply(time,self.v))
 
+    def move(self, time):
+        self.pos = np.add(self.pos, np.multiply(time, self.v))
 
     def collide(self, other):
-        if isinstance(other,Wall):
+        if isinstance(other, Wall):
             return other.collide(self)
 
         vis = self.v
         vio = other.v
-        
-        #calculate collision normal vector
-        col_norm = np.divide(np.subtract(self.pos,other.pos),np.linalg.norm(np.subtract(self.pos,other.pos)))
 
-        #project velocities into collision coordinate system
-        sv_par = np.multiply(col_norm, np.dot(self.v,col_norm))
-        ov_par = np.multiply(col_norm,np.dot(other.v,col_norm))
+        # calculate collision normal vector
+        col_norm = np.divide(np.subtract(self.pos, other.pos),
+                             np.linalg.norm(np.subtract(self.pos, other.pos)))
+
+        # project velocities into collision coordinate system
+        sv_par = np.multiply(col_norm, np.dot(self.v, col_norm))
+        ov_par = np.multiply(col_norm, np.dot(other.v, col_norm))
 
         sv_perp = self.v - sv_par
         ov_perp = other.v - ov_par
-
-
-        #perform collision with transformed vectors
+        # perform collision with transformed vectors
         sv_parf = ov_par
         ov_parf = sv_par
 
-        #reconstruct xy velocities from collision components
-        other.v = np.add(ov_perp,ov_parf)
-        self.v = np.add(sv_perp,sv_parf)
+        # reconstruct xy velocities from collision components
+        other.v = np.add(ov_perp, ov_parf)
+        self.v = np.add(sv_perp, sv_parf)
 
-        try:
-            assert np.dot(self.v,self.v) + np.dot(other.v,other.v) <= 1.01 * (np.dot(vis,vis) + np.dot(vio,vio))
-            assert np.dot(self.v,self.v) + np.dot(other.v,other.v) >= 0.99 * (np.dot(vis,vis) + np.dot(vio,vio))
-        except AssertionError:
-            print(np.dot(self.v,self.v) + np.dot(other.v,other.v), np.dot(vis,vis) + np.dot(vio,vio))
-            sys.exit()
+        # return the total momentum change of the collision (should be zero)
+        p_init = np.add(np.multiply(other.mass, vio),
+                        np.multiply(self.mass, vis))
+        p_fin = np.add(np.multiply(other.mass, other.v),
+                       np.multiply(self.mass, self.v))
 
-        #return the total momentum change of the collision (should be zero)
-        p_init = np.add(np.multiply(other.mass, vio),np.multiply(self.mass,vis))
-        p_fin = np.add(np.multiply(other.mass,other.v),np.multiply(self.mass,self.v))
+        return np.subtract(p_fin, p_init)
 
-        return np.subtract(p_fin,p_init)
+    # predict when a collision will occur between self and other
+    def forecast(self, other):
 
-    #predict when a collision will occur between self and other
-    def forecast(self,other):
         if other == self:
             return None
+
         if isinstance(other, Wall):
-            if np.dot(self.v,other.norm) < 0:
-                return None
-            t = (other.pos - self.radius - np.dot(self.pos,other.norm)) / np.dot(self.v,other.norm)
-        else:
-            if np.dot(np.subtract(self.v,other.v),np.subtract(self.pos,other.pos)) < 0:
-                return None
+            rel_v = np.dot(self.v, other.norm)
 
-            t = 0
-            fut_poss = np.add(self.pos,np.multiply(t,self.v))
-            fut_poso = np.add(other.pos,np.multiply(t,other.v))
-            dist = np.linalg.norm(np.subtract(fut_poss,fut_poso))
-            min_dist = dist
+            if rel_v <= 0:
+                return None
             
-            #advance time of possible collision until atoms touch
-            while dist > self.radius + other.radius:
-                t+=0.0000001 
-                fut_poss = np.add(self.pos,np.multiply(t,self.v))
-                fut_poso = np.add(other.pos,np.multiply(t,other.v))
-                dist = np.linalg.norm(np.subtract(fut_poss,fut_poso))
+            dist_to_wall = other.pos - np.dot(self.pos,other.norm)
 
-                if dist > min_dist:
-                    return None
-                
-                min_dist = dist
-                
+            if dist_to_wall < self.radius and dist_to_wall >= 0:
+                return 0
 
-        if t >= 0:
-            return t
+            t = (dist_to_wall - self.radius) / rel_v
 
-        return None
+            if t > 0:
+                return t
+
+            return None
+
+        else:
+
+            delta_x = np.subtract(self.pos, other.pos)
+            delta_v = np.subtract(self.v, other.v)
+            d = self.radius + other.radius
+
+            a = np.dot(delta_v, delta_v)
+            b = np.dot(delta_x, delta_v)
+            c = np.dot(delta_x, delta_x) - d**2
+            det = b**2 - a * c
+
+            if b > 0:
+                return None
+            elif det < 0:
+                return None
+            elif c < 0:
+                return 0
+            else:
+                return (-b + math.sqrt(det)) / a
--- gas_sim.py
+++ gas_sim.py
@@ -3,36 +3,46 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import math
+import scipy.stats
 from collideable import Atom, Wall
 
-def run_sim(duration, num_atoms, volume, energy, mass, radius):
+
+def run_sim(duration, num_atoms, volume, energy, mass, radius, mb_dis):
     walls = build_walls(volume)
-    atoms = build_atoms(num_atoms, energy, volume, mass, radius)
+    atoms = build_atoms(num_atoms, energy, volume, mass, radius, mb_dis)
     print("finding events")
     event_queue = find_events(atoms, walls)
-    col_dict = {} #dictionary that stores number of collisions for each particle for the
-                        #purpose of invalidating events that will not occur
+    col_dict = {}  # dictionary that stores number of collisions for each particle for the
+    # purpose of invalidating events that will not occur
     for atom in atoms:
         col_dict[atom] = 0
     sim_time = 0
     num_events = 0
 
     #position = np.array(get_all_pos(atoms))
-    #sns.set_style('darkgrid')
-    #sns.scatterplot(position[:,0],position[:,1])
-    #plt.xlim([-volume**(1/3),volume**(1/3)])
-    #plt.ylim([-volume**(1/3),volume**(1/3)])
-    #plt.title(str(sim_time))
+    # sns.set_style('darkgrid')
+    # sns.scatterplot(position[:,0],position[:,1])
+    # plt.xlim([-volume**(1/3),volume**(1/3)])
+    # plt.ylim([-volume**(1/3),volume**(1/3)])
+    # plt.title(str(sim_time))
 
-    #plt.savefig("pos/0.png")
+    # plt.savefig("pos/0.png")
 
-    num_events = 1
+    delta_p_tot = 0
+
+    s = get_all_speed(atoms)
+    sns.set_style('darkgrid')
+    plt.figure()
+    sns.distplot(s)
+    plt.show()
 
     while sim_time < duration:
-        #get next event
+        print(f"Simulating. t = {sim_time:.3f}",end='\r')
+        # get next event
         event = heapq.heappop(event_queue)
 
-        #extract event details
+        # extract event details
         p1, p2, with_wall, pred_cols = event.collision
         col_time = event.t
 
@@ -40,136 +50,151 @@ def run_sim(duration, num_atoms, volume, energy, mass, radius):
         if not with_wall:
             p_cols += col_dict[p2]
 
-        #if either particle has collided since this event was predicted, collision is no longer valid
+        # if either particle has collided since this event was predicted, collision is no longer valid
         if p_cols > pred_cols:
             continue
+
         num_events += 1
 
-        if not with_wall:
-            #for debugging and to get a sense of progress when simulation is running
-            print("#########",col_time, p1, p2)
-        else:
-            #print(col_time,p1,p2)
-            pass
-
-        #advance state of simulation to the time that the event should occur
-        move_all(atoms,sim_time,col_time)
+        # advance state of simulation to the time that the event should occur
+        move_all(atoms, sim_time, col_time)
         sim_time = col_time
 
-        #perform collision
-        p1.collide(p2)
+        # perform collision
+        delta_p = p1.collide(p2)
 
-        #update number of collisions for each atom involved in the event
+        if with_wall:
+            delta_p_tot += delta_p
+
+        # update number of collisions for each atom involved in the event
         col_dict[p1] += 1
         if not with_wall:
             col_dict[p2] += 1
 
-        #predict new events for involved atoms
-        refind_events(event_queue,p1,atoms,walls,sim_time,col_dict, ignore=p2)
+        # predict new events for involved atoms
+        refind_events(event_queue, p1, atoms, walls,
+                      sim_time, col_dict, ignore=p2)
         if not with_wall:
-            refind_events(event_queue,p2,atoms,walls,sim_time,col_dict,ignore=p1)
+            refind_events(event_queue, p2, atoms, walls,
+                          sim_time, col_dict, ignore=p1)
 
-        #Plotting code for purpose of making animation
+        # Plotting code for purpose of making animation
         #position = np.array(get_all_pos(atoms))
-        #plt.cla()
-        #sns.scatterplot(x=position[:,0],y=position[:,1])
-        #plt.xlim([-volume**(1/3),volume**(1/3)])
-        #plt.ylim([-volume**(1/3),volume**(1/3)])
-        #plt.title(str(sim_time))
+        # plt.cla()
+        # sns.scatterplot(x=position[:,0],y=position[:,1])
+        # plt.xlim([-volume**(1/3),volume**(1/3)])
+        # plt.ylim([-volume**(1/3),volume**(1/3)])
+        # plt.title(str(sim_time))
         #name = "pos/" + str(num_events)
         #name = name + ".png"
-        #plt.savefig(name)
-        
-    #Show distribution of particle speeds after sim has run
+        # plt.savefig(name)
+
+    print(f"\n{num_events} events occurred",end='\n\n')
+
+    # Show distribution of particle speeds after sim has run
     s = get_all_speed(atoms)
-    print(s)
-    print(num_events)
     sns.set_style('darkgrid')
     plt.figure()
     sns.distplot(s)
     plt.show()
 
-                
+    #calculate pressure
+    p = (delta_p_tot / sim_time) / (6 * volume**(2/3))
+
+    print(f"PV: {p * volume}")
+
+    #calculate energy
+    U = 0
+    for speed in s:
+        U += speed**2 * mass / 2
+
+    print(f"NkT: {U * 2 / 3}")
 
 
-    
-#builds distribution of atoms in center-ish of box with same velocity in random direction
-def build_atoms(num_atoms, energy, volume,mass, radius):
-    vel = (energy * 2 / mass / num_atoms)**(1/2)
+
+
+# builds distribution of atoms in center-ish of box with same velocity in random direction
+def build_atoms(num_atoms, energy, volume, mass, radius, mb_dis):
+    if mb_dis:
+        a = math.sqrt(2 * energy / (3 * num_atoms * mass))
+        vels = scipy.stats.maxwell.rvs(scale=a,size=num_atoms)
+    else:
+        vels = [(energy * 2 / mass / num_atoms)**(1/2) for i in range(num_atoms)]
     atoms = []
-    max_coord = (volume)**(1/3) / 1.2 #all particles will start in the middle of the box
+    # all particles will start in the middle of the box
+    max_coord = (volume)**(1/3) / 2 - radius
     positions = []
 
-    for _ in range(num_atoms):
-        #get random direction and position
+    for i in range(num_atoms):
+        # get random direction and position
         dir = [random.random() - 0.5 for j in range(3)]
-        dir_unit = np.divide(dir,np.linalg.norm(dir))
-        pos = [(random.random()-0.5)* 2 * max_coord for j in range(3)]
-        
-        #don't allow atoms to start inside of eachother
-        while intersects(pos,positions,radius):
-           print("changing position")
-           pos = [(random.random()-0.5)* 2 * max_coord for j in range(3)]
-           
+        dir_unit = np.divide(dir, np.linalg.norm(dir))
+        pos = [(random.random()-0.5) * 2 * max_coord for j in range(3)]
+
+        # don't allow atoms to start inside of eachother
+        while intersects(pos, positions, radius):
+            print(f"changing position of particle {i}")
+            pos = [(random.random()-0.5) * 2 * max_coord for j in range(3)]
+
         positions.append(pos)
 
-        atoms.append(Atom(mass,radius,pos,np.multiply(dir_unit,vel)))
-
+        atoms.append(Atom(mass, radius, pos, np.multiply(dir_unit, vels[i])))
 
     return atoms
 
 
 def build_walls(volume):
-    #create walls that bound a cube with given volume
-    pos = volume**(1/3)
+    # create walls that bound a cube with given volume
+    pos = volume**(1/3) / 2
     walls = []
-    walls.append(Wall([1,0,0],pos))
-    walls.append(Wall([-1,0,0],pos))
-    walls.append(Wall([0,1,0],pos))
-    walls.append(Wall([0,-1,0],pos))
-    walls.append(Wall([0,0,1],pos))
-    walls.append(Wall([0,0,-1],pos))
+    walls.append(Wall([1, 0, 0], pos))
+    walls.append(Wall([-1, 0, 0], pos))
+    walls.append(Wall([0, 1, 0], pos))
+    walls.append(Wall([0, -1, 0], pos))
+    walls.append(Wall([0, 0, 1], pos))
+    walls.append(Wall([0, 0, -1], pos))
 
     return walls
 
-#Builds priority queue of events
+# Builds priority queue of events
 def find_events(atoms, walls):
     event_queue = []
     for idx, atom1 in enumerate(atoms):
-        print("finding events for", atom1)
         for atom2 in atoms[idx:]:
             t = atom1.forecast(atom2)
             if t is not None:
-                heapq.heappush(event_queue,Event(t,(atom1,atom2,False,0)))
+                heapq.heappush(event_queue, Event(t, (atom1, atom2, False, 0)))
 
         for wall in walls:
             t = atom1.forecast(wall)
             if t is not None:
-                heapq.heappush(event_queue,Event(t,(atom1,wall,True,0)))
+                heapq.heappush(event_queue, Event(t, (atom1, wall, True, 0)))
 
-    for event in event_queue:
-        print(event.t, event.collision[0].pos,event.collision[1].pos,event.collision[2])
     return event_queue
 
-def refind_events(event_queue,atom, atoms, walls,cur_time,col_dict, ignore=None):
+
+def refind_events(event_queue, atom, atoms, walls, cur_time, col_dict, ignore=None):
     for atom2 in atoms:
         if atom2 == ignore:
             continue
-        t = atom.forecast(atom2) 
+        t = atom.forecast(atom2)
         if t is not None:
-            heapq.heappush(event_queue, Event(cur_time+t,(atom,atom2,False,col_dict[atom]+col_dict[atom2])))
-        
+            heapq.heappush(event_queue, Event(
+                cur_time+t, (atom, atom2, False, col_dict[atom]+col_dict[atom2])))
+
     for wall in walls:
         t = atom.forecast(wall)
 
         if t is not None:
-            heapq.heappush(event_queue,Event(cur_time+t,(atom,wall,True,col_dict[atom])))
+            heapq.heappush(event_queue, Event(
+                cur_time+t, (atom, wall, True, col_dict[atom])))
 
 def move_all(atoms, cur_time, end_time):
     # update positions of all atoms
     time = end_time - cur_time
     for atom in atoms:
         atom.move(time)
+
 
 def get_all_speed(atoms):
     s = []
@@ -178,11 +203,18 @@ def get_all_speed(atoms):
 
     return s
 
+
 def get_all_pos(atoms):
     p = []
     for atom in atoms:
         p.append(atom.pos)
     return p
+
+def intersects(pos, positions, radius):
+    for position in positions:
+        if np.linalg.norm(np.subtract(position, pos)) < 2 * radius:
+            return True
+    return False
 
 class Event:
     def __init__(self, t, collision):
@@ -191,12 +223,7 @@ class Event:
 
     def __lt__(self, other):
         return self.t < other.t
-        
-def intersects(pos, positions,radius):
-    for position in positions:
-        if np.linalg.norm(np.subtract(position,pos)) < 2 * radius:
-            return True
-    return False
 
-#Run the simulation
-run_sim(1000,800,600,100,2,0.5)
+
+# Run the simulation
+run_sim(100, 100, 200, 100, 0.1, 0.1, True)
